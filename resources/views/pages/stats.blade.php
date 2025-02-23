26df948e5810f45df6b0ee04b0b7e06c03d352d3
nikajibladze <nika8463@gmail.com>
2025-02-23 22:51:27 +0400

    jandacva

--- resources/views/pages/stats.blade.php
+++ resources/views/pages/stats.blade.php
@@ -105,8 +105,130 @@
                             <div class="blog-item">
                                 <div class="cont">
                                     <div class="tags d-flex justify-content-between">
+                                        <a href="#"> ჯანდაცვის სამინისტრო </a>
+                                        <a href="#" class="badge bg-danger"> მაღალი პროგრესი </a>
+                                    </div>
+                                    <h5> <a href="#"> 2024 წლის II კვარტლის ანგარიში </a> </h5>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="stats-small-title fw-bold mb-3">პროექტების სტატუსი</div>
+                                            <div class="skills mt-5 mt-lg-0">
+                                                <div class="progress-item mb-2">
+                                                    <div class="txt"> რეფერენტული სისტემა </div>
+
+                                                    <div class="progress">
+                                                        <div class="progress-bar bg-success" role="progressbar"
+                                                            style="width: 25%;" aria-valuenow="25" aria-valuemin="0"
+                                                            aria-valuemax="100">25%
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="progress-item mb-2">
+                                                    <div class="txt">კლინიკების რეაბილიტაცია</div>
+
+                                                    <div class="progress">
+                                                        <div class="progress-bar bg-warning" role="progressbar"
+                                                            style="width: 19%;" aria-valuenow="19" aria-valuemin="0"
+                                                            aria-valuemax="19">19%
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="progress-item mb-2">
+                                                    <div class="txt"> ექიმების გადამზადება</div>
+
+                                                    <div class="progress">
+                                                        <div class="progress-bar" role="progressbar" style="width: 25%;"
+                                                            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="stats-small-title fw-bold  mb-3">ბიუჯეტის განაწილება</div>
+                                            <div class="stat-counter">
+                                                <div class="stats-parent d-flex justify-content-between mb-3">
+                                                    <span>ინფრასტრუქტურა</span>
+                                                    <span class="fw-bold">4.2M ₾</span>
+                                                </div>
+
+                                                <div class="stats-parent d-flex justify-content-between mb-3">
+                                                    <span>ტრეინინგები</span>
+                                                    <span class="fw-bold">1.2M ₾</span>
+                                                </div>
+
+                                                <div class="stats-parent d-flex justify-content-between mb-3">
+                                                    <span>ტექნიკური აღჭრვილობა</span>
+                                                    <span class="fw-bold">2.2M ₾</span>
+                                                </div>
+
+                                                <div class="stats-parent d-flex justify-content-between mb-3">
+                                                    <span>კვლევები</span>
+                                                    <span class="fw-bold">0.8M ₾</span>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div class="stats-main-info">
+                                        <div class="stats-small-title fw-bold mb-3">მთავარი მიღწევები</div>
+
+
+
+                                        <div class="row">
+                                            <div class="col-lg-12">
+
+                                                <ul>
+                                                    <li>
+                                                        <a href="#"> <i class="far fa-angle-right icon mb-2"></i>
+                                                            შემცირდა მედიკამენტის ფასები</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"> <i class="far fa-angle-right icon mb-2"></i>
+                                                            500 მა ექიმმა გაიარა ციფრული უნარების ტრეინინგი </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"> <i class="far fa-angle-right icon mb-2"></i>
+                                                            დასრულდა 30 კლინიკის სრული რეაბილიტაცია</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"> <i class="far fa-angle-right icon mb-2"></i>
+                                                            შეიქმნა
+                                                            3 ახალი ონლაინ საგანმანათლებლო პროგრამა </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                    <div class="stats-footer mt-3">
+                                        <div class="footer-actions d-flex justify-content-between">
+                                            <div class="footer-action-item">
+                                                <a href="#" class="badge bg-danger"> სრული ანგარიშის ნახვა PDF ში
+                                                </a>
+                                            </div>
+                                            <div class="tags">
+                                                <a href="#"> გაზიარება </a>
+                                                <a href="#" class="badge bg-success"> შენიშვნის დამატება </a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="blog-item">
+                                <div class="cont">
+                                    <div class="tags d-flex justify-content-between">
                                         <a href="#"> განათლების სამინისტრო </a>
-                                        <a href="#" class="badge bg-success"> მაღალი პროგრესი </a>
+                                        <a href="#" class="badge bg-warning"> საშუალო პროგრესი </a>
                                     </div>
                                     <h5> <a href="#"> 2024 წლის I კვარტლის ანგარიში </a> </h5>
                                     <div class="row">
